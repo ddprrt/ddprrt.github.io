518b9bd407f4d8de338cce45fe9efe7d3d375e11
Baumgartner <sbaumg@gmail.com>
2020-05-28 12:53:22 +0200

    try this out

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const { task, src, dest} = require('gulp')
 const svg2png = require('gulp-svg2png')
+const newy = require('gulp-newy')
+const path = require('path')
+
+function checkPng(projectDir, srcFile, absSrc) {
+  const p = absSrc.replace('.svg', '.png')
+  console.log(p)
+  return p
+}
 
 task('default', () => {
   return src('./dist/teasers/**.svg')
+    .pipe(newy(checkPng))
     .pipe(svg2png())
     .pipe(dest('./dist/teasers'))
 })
