d367ba96ce296153a169742c65caa8a180cf629e
Stefan Baumgartner <sbaumg@gmail.com>
2020-07-24 16:51:14 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -150,7 +150,7 @@ module.exports = function(config) {
   config.addPassthroughCopy('./src/pwabuilder-sw.js')
   config.addPassthroughCopy({
     './src/content/typescript-react/img/': 'typescript-react/img/',
-    './src/brave-payment-verification.txt': '.well-known/brave-payment-verification.txt'
+    './src/brave-payment-verification.txt': 'brave-payment-verification.txt'
   })
   config.addPassthroughCopy('./src/googlea8ba377bc3684d37.html')
   config.addPassthroughCopy('./src/robots.txt')
