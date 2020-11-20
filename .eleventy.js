4163d9d9fa2fc3de702221084ebd02ec692a6239
Baumgartner <sbaumg@gmail.com>
2020-11-20 10:41:39 +0100

    replaceStart filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,173 +1,186 @@
 //@ts-nocheck
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
-const dateformat = require('dateformat')
-const markdownIt = require('markdown-it')
-const inlineMd = markdownIt()
-const markdownItAnchor = require('markdown-it-anchor')
-const markdownItLazy =  require('markdown-it-image-lazy-loading')
-const cheerio = require('cheerio')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const dateformat = require("dateformat");
+const markdownIt = require("markdown-it");
+const inlineMd = markdownIt();
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItLazy = require("markdown-it-image-lazy-loading");
+const cheerio = require("cheerio");
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   /*config.addFilter('cssmin', function(code) {
     return new CleanCSS({}).minify(code).styles;
   })*/
 
-  config.addFilter('year', function() {
-    return new Date().getFullYear()
-  })
+  config.addFilter("year", function () {
+    return new Date().getFullYear();
+  });
 
-  config.addFilter('getYear', function(input) {
-    return new Date(input).getFullYear()
-  })
+  config.addFilter("getYear", function (input) {
+    return new Date(input).getFullYear();
+  });
 
-  config.addFilter('prettyDate', function(input) {
-    return dateformat(new Date(input), 'mmmm d, yyyy')
-  })
+  config.addFilter("prettyDate", function (input) {
+    return dateformat(new Date(input), "mmmm d, yyyy");
+  });
 
-  config.addFilter('toISO', function(input) {
-    return new Date(input).toISOString()
-  })
+  config.addFilter("toISO", function (input) {
+    return new Date(input).toISOString();
+  });
 
-  config.addFilter('readingTime', function(input) {
-    const words = input.split(' ').filter(el => el !== '').length
-    return parseInt(words / 200 + 0.5)
-  })
+  config.addFilter("readingTime", function (input) {
+    const words = input.split(" ").filter((el) => el !== "").length;
+    return parseInt(words / 200 + 0.5);
+  });
 
-  config.addFilter('keys', function(input) {
-    return JSON.stringify(Object.keys(input))
-  })
+  config.addFilter("keys", function (input) {
+    return JSON.stringify(Object.keys(input));
+  });
 
-  config.addFilter('getGuide', function(tags) {
-    return tags.filter(elem => elem !== 'guides')
-      .filter(elem => elem.startsWith('guides'))
-      .pop()
-  })
+  config.addFilter("getGuide", function (tags) {
+    return tags
+      .filter((elem) => elem !== "guides")
+      .filter((elem) => elem.startsWith("guides"))
+      .pop();
+  });
 
-  config.addFilter('byOrder', function(coll) {
-    return coll.sort((a, b) => a.data.order - b.data.order)
-  })
+  config.addFilter("byOrder", function (coll) {
+    return coll.sort((a, b) => a.data.order - b.data.order);
+  });
 
-  config.addFilter('markdownify', function(input) {
-    return inlineMd.render(input)
-  })
+  config.addFilter("markdownify", function (input) {
+    return inlineMd.render(input);
+  });
 
-  config.addFilter('log', function(input) {
-    console.log('🤡', input)
-    return input
-  })
+  config.addFilter("log", function (input) {
+    console.log("🤡", input);
+    return input;
+  });
 
-
-  config.addFilter('splitlines', function(input) {
-    const parts = input.split(' ')
-    const lines = parts.reduce(function(prev, current) {
-      if(!prev.length) {
-        return [current]
+  config.addFilter("splitlines", function (input) {
+    const parts = input.split(" ");
+    const lines = parts.reduce(function (prev, current) {
+      if (!prev.length) {
+        return [current];
       }
-      let lastOne = prev[prev.length - 1]
-      if(lastOne.length + current.length > 18) {
-        return [...prev, current]
+      let lastOne = prev[prev.length - 1];
+      if (lastOne.length + current.length > 18) {
+        return [...prev, current];
       }
-      prev[prev.length - 1] = lastOne + ' ' + current
-      return prev
-    }, [])
-    return lines
-  })
+      prev[prev.length - 1] = lastOne + " " + current;
+      return prev;
+    }, []);
+    return lines;
+  });
 
-  config.addFilter('abstract', function(input) {
-    const $ = cheerio.load(input)
-    const x = $('p:first-of-type').eq(0).toString()
-    const y = $('p:first-of-type ~ :not(h2, h3, h4, ol, li, ul)').eq(0).toString()
-    return x + y
-  })
+  config.addFilter("abstract", function (input) {
+    const $ = cheerio.load(input);
+    const x = $("p:first-of-type").eq(0).toString();
+    const y = $("p:first-of-type ~ :not(h2, h3, h4, ol, li, ul)")
+      .eq(0)
+      .toString();
+    return x + y;
+  });
 
+  config.addFilter("excerpt", function (input) {
+    const firstParagraph = input.indexOf("<p>");
+    const firstParagraphEnd = input.indexOf("</p>");
+    return `<p>${input.substring(firstParagraph + 3, firstParagraphEnd)}</p>`;
+  });
 
-  config.addFilter('excerpt', function(input) {
-    const firstParagraph = input.indexOf('<p>')
-    const firstParagraphEnd = input.indexOf('</p>')
-    return `<p>${input.substring(firstParagraph + 3, firstParagraphEnd)}</p>`
-  })
+  config.addFilter("filterTOC", function (input) {
+    return input.filter((el) => !el.data.excludeFromTOC);
+  });
 
-  config.addFilter('filterTOC', function(input) {
-    return input.filter(el => !el.data.excludeFromTOC)
-  })
-
-  config.addShortcode('now', function() {
-    return new Date().toISOString()
-  })
-
-  config.addNunjucksFilter('toLowerCase', function(value) {
-    return value.toLowerCase()
-  })
-
-  config.addNunjucksFilter('teaserIMG', function(value) {
-    const { fileSlug } = value
-    const { permalink } = value.data
-    if(permalink && permalink.includes('page.fileSlug')) {
-      console.log('👍', permalink, fileSlug)
-      return `/${fileSlug}/`
+  config.addFilter("replaceStart", function (input, ...args) {
+    if (input.startsWith(args[0])) {
+      return input.replace(...args);
     }
-    return permalink || ''
-  })
+    return input;
+  });
 
-  config.addCollection('categories', function (collection) {
-    const categories = collection.items.filter(Boolean).reverse().reduce((prev, current) => {
-      if(current.data.categories) {
-        return [...new Set([...current.data.categories, ...prev])]
-      }
-      return prev
-    }, [])
-    let cats = {}
-    categories.forEach(el => {
+  config.addShortcode("now", function () {
+    return new Date().toISOString();
+  });
+
+  config.addNunjucksFilter("toLowerCase", function (value) {
+    return value.toLowerCase();
+  });
+
+  config.addNunjucksFilter("teaserIMG", function (value) {
+    const { fileSlug } = value;
+    const { permalink } = value.data;
+    if (permalink && permalink.includes("page.fileSlug")) {
+      console.log("👍", permalink, fileSlug);
+      return `/${fileSlug}/`;
+    }
+    return permalink || "";
+  });
+
+  config.addCollection("categories", function (collection) {
+    const categories = collection.items
+      .filter(Boolean)
+      .reverse()
+      .reduce((prev, current) => {
+        if (current.data.categories) {
+          return [...new Set([...current.data.categories, ...prev])];
+        }
+        return prev;
+      }, []);
+    let cats = {};
+    categories.forEach((el) => {
       cats[el] = Array.from(collection.items)
-        .filter(item => !item.data.hideFromList)
-        .filter(item => item.data.categories && item.data.categories.includes(el))
+        .filter((item) => !item.data.hideFromList)
+        .filter(
+          (item) => item.data.categories && item.data.categories.includes(el)
+        )
         .sort((a, b) => {
-          return a.date - b.date
-        })
-    })
-    return cats
-  })
+          return a.date - b.date;
+        });
+    });
+    return cats;
+  });
 
-  config.addPlugin(syntaxHighlight)
+  config.addPlugin(syntaxHighlight);
 
   config.setLibrary(
-    'md',
+    "md",
     markdownIt({
       html: true,
       breaks: false,
       linkify: true,
-      typographer: true
-    }).use(markdownItAnchor, {
-      permalink: true,
-      permalinkClass: "direct-link",
-      permalinkSymbol: "#"
-    }).use(markdownItLazy)
-  )
+      typographer: true,
+    })
+      .use(markdownItAnchor, {
+        permalink: true,
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#",
+      })
+      .use(markdownItLazy)
+  );
 
-  config.addPassthroughCopy('./src/wp-content/')
-  config.addPassthroughCopy('./src/manifest.json')
-  config.addPassthroughCopy('./src/pwabuilder-sw.js')
+  config.addPassthroughCopy("./src/wp-content/");
+  config.addPassthroughCopy("./src/manifest.json");
+  config.addPassthroughCopy("./src/pwabuilder-sw.js");
   config.addPassthroughCopy({
-    './src/content/typescript-react/img/': 'typescript-react/img/'
-  })
-  config.addPassthroughCopy('./src/googlea8ba377bc3684d37.html')
-  config.addPassthroughCopy('./src/robots.txt')
-  config.addPassthroughCopy('./src/icon/')
-  config.addWatchTarget('./src/_assets')
-
+    "./src/content/typescript-react/img/": "typescript-react/img/",
+  });
+  config.addPassthroughCopy("./src/googlea8ba377bc3684d37.html");
+  config.addPassthroughCopy("./src/robots.txt");
+  config.addPassthroughCopy("./src/icon/");
+  config.addWatchTarget("./src/_assets");
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
-      data: '_data',
-      includes: '_includes',
-      layouts: '_layouts'
+      input: "src",
+      output: "dist",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts",
     },
-    templateFormats : ['njk', 'md', '11ty.js'],
-    htmlTemplateEngine : 'njk',
-    markdownTemplateEngine : 'njk',
+    templateFormats: ["njk", "md", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
     passthroughFileCopy: true,
-  }
-}
+  };
+};
