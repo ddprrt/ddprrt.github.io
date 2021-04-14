1147c9c418ab99ac57ffb58d77a0312d3ca22b5f
Baumgartner <sbaumg@gmail.com>
2021-04-14 13:03:46 +0200

    A test

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -1,3 +1,4 @@
+/// <reference lib="webworker" />
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 //Install stage sets up the offline page in the cahche and opens a new cache
@@ -5,15 +6,48 @@ self.addEventListener("install", function (event) {
   event.waitUntil(preLoad());
 });
 
-var preLoad = function () {
-  console.log("[PWA Builder] Install Event processing");
+function preLoad() {
   return caches.open("pwabuilder-offline").then(function (cache) {
-    console.log("[PWA Builder] Cached index and offline page during Install");
-    return cache.addAll(["/404.html", "/index.html"]);
+    return cache.addAll([
+      "/assets/styles/main.css?v=5",
+      "/assets/scripts/main.js?v=5",
+      "https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Source+Serif+Pro:wght@400;600;700&display=swap",
+      "/wp-content/uploads/me.jpg",
+      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIQzD-0qpwxpaWvjeD0X88SAOeauXQ-oAGIyY0.woff2",
+      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIXzD-0qpwxpaWvjeD0X88SAOeasasatSyqxKcsdrM.woff2",
+      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIXzD-0qpwxpaWvjeD0X88SAOeasc8btSyqxKcsdrM.woff2",
+      "/icon/favicon-32x32.png",
+      "/icon/apple-icon-180x180.png",
+    ]);
   });
-};
+}
 
-self.addEventListener("fetch", function (event) {
+/**
+ *
+ * @param {Request} request
+ * @returns
+ */
+function returnFromCache(request) {
+  return caches.open("pwabuilder-offline").then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+/**
+ *
+ * @param {FetchEvent} event
+ */
+function fetchEventHandler(event) {
+  event.respondWith(
+    returnFromCache(event.request).catch(function () {
+      return fetch(event.request);
+    })
+  );
+}
+
+self.addEventListener("fetch", fetchEventHandler);
+
+/*self.addEventListener("fetch", function (event) {
   console.log("The service worker is serving the asset. ", event.request.url);
   event.respondWith(
     checkResponse(event.request).catch(function () {
@@ -55,3 +89,4 @@ var returnFromCache = function (request) {
     });
   });
 };
+*/
