a5bf51c436bde96584640fd98abed6073a162049
Baumgartner <sbaumg@gmail.com>
2021-04-14 14:07:47 +0200

    my pwa stuff was seriously broken

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -1,109 +1,76 @@
 /// <reference lib="webworker" />
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Install stage sets up the offline page in the cahche and opens a new cache
+const version = "6";
+
+var offlineFundamentals = [
+  "",
+  `/assets/styles/main.css?v=${version}`,
+  `/assets/scripts/main.js?v=${version}`,
+];
+
 self.addEventListener("install", function (event) {
-  event.waitUntil(preLoad());
-});
-
-function preLoad() {
-  return caches.open("pwabuilder-offline").then(function (cache) {
-    return cache.addAll([
-      "/assets/styles/main.css?v=5",
-      "/assets/scripts/main.js?v=5",
-      "https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Source+Serif+Pro:wght@400;600;700&display=swap",
-      "/wp-content/uploads/me.jpg",
-      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIQzD-0qpwxpaWvjeD0X88SAOeauXQ-oAGIyY0.woff2",
-      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIXzD-0qpwxpaWvjeD0X88SAOeasasatSyqxKcsdrM.woff2",
-      "https://fonts.gstatic.com/s/sourceserifpro/v11/neIXzD-0qpwxpaWvjeD0X88SAOeasc8btSyqxKcsdrM.woff2",
-      "/icon/favicon-32x32.png",
-      "/icon/apple-icon-180x180.png",
-    ]);
-  });
-}
-
-/**
- *
- * @param {Request} request
- * @returns
- */
-function returnFromCache(request) {
-  return caches.open("pwabuilder-offline").then(function (cache) {
-    return cache.match(request);
-  });
-}
-
-/**
- *
- * @param {FetchEvent} event
- */
-function fetchEventHandler(event) {
-  event.respondWith(
-    returnFromCache(event.request).catch(function () {
-      return checkResponse(event.request);
+  event.waitUntil(
+    caches.open(version + "::fundamentals").then(function (cache) {
+      return cache.addAll(offlineFundamentals);
     })
   );
-}
-
-/**
- *
- * @param {Request} request
- * @returns
- */
-function checkResponse(request) {
-  return new Promise(function (fulfill, reject) {
-    fetch(request).then(function (response) {
-      if (response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  });
-}
-
-self.addEventListener("fetch", fetchEventHandler);
-
-/*self.addEventListener("fetch", function (event) {
-  console.log("The service worker is serving the asset. ", event.request.url);
-  event.respondWith(
-    checkResponse(event.request).catch(function () {
-      return returnFromCache(event.request);
-    })
-  );
-  event.waitUntil(addToCache(event.request));
 });
 
-var checkResponse = function (request) {
-  return new Promise(function (fulfill, reject) {
-    fetch(request).then(function (response) {
-      if (response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  });
-};
+self.addEventListener("fetch", function (/** @type {FetchEvent} */ event) {
+  if (
+    event.request.method !== "GET" ||
+    event.request.url.includes("carbonads") ||
+    event.request.url.includes("buysellads") ||
+    event.request.url.includes("doubleclick")
+  ) {
+    return;
+  }
 
-var addToCache = function (request) {
-  return caches.open("pwabuilder-offline").then(function (cache) {
-    return fetch(request).then(function (response) {
-      console.log("[PWA Builder] add page to offline: " + response.url);
-      return cache.put(request, response);
-    });
-  });
-};
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      const networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
 
-var returnFromCache = function (request) {
-  return caches.open("pwabuilder-offline").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status == 404) {
-        return cache.match("404.html");
-      } else {
-        return matching;
+      return cached || networked;
+
+      function fetchedFromNetwork(response) {
+        const cacheCopy = response.clone();
+        caches
+          // We open a cache to store the response for this request.
+          .open(version + "::pages")
+          .then(function add(cache) {
+            return cache.put(event.request, cacheCopy);
+          });
+
+        return response;
       }
-    });
-  });
-};
-*/
+
+      function unableToResolve() {
+        return new Response("<h1>Service Unavailable</h1>", {
+          status: 503,
+          statusText: "Service Unavailable",
+          headers: new Headers({
+            "Content-Type": "text/html",
+          }),
+        });
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return !key.startsWith(`${version}::`);
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+});
