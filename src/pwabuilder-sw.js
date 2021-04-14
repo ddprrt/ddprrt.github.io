f9e40d31bbfc4fdafdfaafcd71b263f26befe320
Baumgartner <sbaumg@gmail.com>
2021-04-14 12:35:12 +0200

    for now

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -14,7 +14,7 @@ var preLoad = function () {
 };
 
 self.addEventListener("fetch", function (event) {
-  console.log("The service worker is serving the asset.", event.request.url);
+  console.log("The service worker is serving the asset. ", event.request.url);
   event.respondWith(
     checkResponse(event.request).catch(function () {
       return returnFromCache(event.request);
